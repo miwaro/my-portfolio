d34ca9845c9e632a93344ee4a2a6c0b6446e2b58
Michael <mikerooze12@gmail.com>
2020-12-09 04:14:47 -0800

    Update skills section

--- src/components/Skills.js
+++ src/components/Skills.js
@@ -6,21 +6,10 @@ const Skills = () => {
             <div>
                 <h1 className='skills-title'>Skills</h1>
             </div>
-            <div className="logos">
-                <img src='images/redux.png' className='contact-githubImage'></img>
-                <img src='images/html.svg' className='contact-githubImage'></img>
-                <img src='images/j.png' className='contact-githubImage'></img> 
-                <img src='images/typescript.webp' className='contact-githubImage'></img> 
-                <img src='images/react.png' className='contact-githubImage'></img>
-                <img src='images/material.png' className='contact-githubImage'></img> 
-                <img src='images/bootstrap-stack.png' className='contact-githubImage'></img> 
-                <img src='images/scss.jpg' className='contact-githubImage'></img> 
-                <img src='images/jest.jpg' className='contact-githubImage'></img>
-                <img src='images/express.png' className='contact-githubImage'></img> 
-            </div>
+
 
             <div className='about-listContainer'>
-                <div>
+                <div className="frames">
                     <p className='about-subheading'>
                         Frameworks &amp; <br></br>Languages
                     </p>
@@ -28,46 +17,53 @@ const Skills = () => {
                         <li>React<br /></li>
                         <li>Redux<br /></li>
                         <li>HTML5<br /></li>
-                        <li>CSS3<br /></li>
+                        <li>CSS/SCSS<br /></li>
                         <li>Javascript<br /></li>
                         <li>Typescript<br /></li>
                         <li>NodeJS<br /></li>
                         <li>Express<br /></li>
                         <li>Material-UI<br /></li>
                         <li>Bootstrap<br /></li>
-                        <li>SCSS<br /></li>
                         <li>Jest<br /></li>
                     </p>
                 </div>
 
-                <div>
+                <div className="tools">
                     <p className='about-subheading'>
                         Dev-Tools
                     </p>
-                    <p className='about-list'>
-                        <li>Webpack<br /></li>
-                        <li>Babel<br /></li>
-                        <li>Git and Github<br /></li>
-                        <li>Bash<br /></li>
-                        <li>Heroku<br /></li>
-                        <li>Google Domains<br /></li>
-                        <li>Chrome Dev-Tools<br /></li>
-                    </p>
+                    <li>Webpack<br /></li>
+                    <li>Babel<br /></li>
+                    <li>Git and Github<br /></li>
+                    <li>Bash<br /></li>
+                    <li>Heroku<br /></li>
+                    <li>Google Domains<br /></li>
+                    <li>Chrome Dev-Tools<br /></li>
                 </div>
 
-                <div>
+                <div className="certs">
                     <p className='about-subheading'>
                         Courses &amp; Certificates
-                    </p>
-                    <p className='about-list'>
-                        <li>Team Treehouse - Front-End Web-Dev Certificate<br /></li>
-                        <li>Udemy - The Complete React Web Dev Course<br /></li>
-                        <li>Udemy - The Complete Javascript Course 2018-2020<br /></li>
-                        <li>Udemy - Understanding Typescript<br /></li>
-                        <li>Udemy - The Complete Node.js Developer Course</li>
-                        <li>Udemy - Bootstrap 4 From Scratch<br /></li>
-                        <li>Udemy - Ultimate CSS Grid Course<br /></li>
-                    </p>
+                    </p>    
+                    <li>Team Treehouse - Front-End Web-Dev Certificate<br /></li>
+                    <li>Udemy - The Complete React Web Dev Course<br /></li>
+                    <li>Udemy - The Complete Javascript Course 2018-2020<br /></li>
+                    <li>Udemy - Understanding Typescript<br /></li>
+                    <li>Udemy - The Complete Node.js Developer Course</li>
+                    <li>Udemy - Bootstrap 4 From Scratch<br /></li>
+                    <li>Udemy - Ultimate CSS Grid Course<br /></li>           
+                </div>
+                <div className="logos">
+                    <img src='images/react.png' className='logos-img'></img>
+                    <img src='images/redux.png' className='logos-img'></img>
+                    <img src='images/html.svg' className='logos-img'></img>
+                    <img src='images/j.png' className='logos-img'></img> 
+                    <img src='images/typescript.webp' className='logos-img'></img> 
+                    <img src='images/material.png' className='logos-img'></img> 
+                    <img src='images/bootstrap-stack.png' className='logos-img'></img> 
+                    <img src='images/scss.jpg' className='logos-img'></img> 
+                    <img src='images/jest.png' className='logos-img'></img>
+                    <img src='images/express.png' className='logos-img'></img> 
                 </div>
             </div>
             <hr></hr>
