b756e99a35ac876ac190cf253d26f3055057bc3b
Michael <mikerooze12@gmail.com>
2020-12-09 04:40:30 -0800

    Update contact section

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -3,34 +3,24 @@ import React from 'react';
 const Contact = () => {
   return (
     <div className='contact-contentContainer'  id="contact">
-        <h1 className='section-title'>Contact</h1>
-        
-        <div className='contact-subtitle'>
-          <p >
-          Interested in working together? Contact me directly by clicking button below!
-          </p>
-          <div className='portfolio-portfolioButtonContainer'>
-            <div className='contact-contactButton'>
-                <a href='mailto:mikerooze12@gmail.com' className='button'>Hire Me</a>
-            </div>
+      <h1 className='section-title'>Contact</h1>
+      <p>Interested in working together? Contact me directly by clicking button below!</p>
+          <div className='contact-contactButton'>
+            <a href='mailto:mikerooze12@gmail.com' className='button'>Hire Me</a>
           </div>
-        </div>
-        
-
-            <div className="linked">
+            <a href='https://www.linkedin.com/in/michael-rooze-b8b356108/' target='blank'>
+              <div className="linked">
               <p>Connect with me on LinkedIn</p>
-              <a href='https://www.linkedin.com/in/michael-rooze-b8b356108/' target='blank'>
-                <img src='images/linked-in.png'></img>
-              </a>
-            </div>
-
-            <div className="git">
-              <p>Check out my repos</p>
-              <a href='https://github.com/miwaro' target='blank'>
+              <img src='images/linked-in.png'></img>
+              </div>
+            </a>
+            <a href='https://github.com/miwaro' target='blank'>
+              <div className="git">
+                <p>Check out my repos</p>
                 <img src='images/github.png' className='contact-githubImage'></img>
-              </a>
-            </div>  
-        </div>
+              </div>  
+            </a>  
+      </div>
   );
 }
 
